6ba26b4a10b4ee3fd78f16d71f07f3832cc48d3c
JeanPierreLeon <jeanp.leons@uqvirtual.edu.co>
2024-09-23 14:36:07 -0500

    Proyecto inicial

---
+++ co.edu.uniquindio.billeteravirtual/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module co.edu.uniquindio.pr3.billeteravirtualapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires org.controlsfx.controls;
+    requires org.mapstruct;
+    requires javafx.graphics;
+
+
+    opens co.edu.uniquindio.pr3.billeteravirtualapp to javafx.fxml;
+    exports co.edu.uniquindio.pr3.billeteravirtualapp;
+}
